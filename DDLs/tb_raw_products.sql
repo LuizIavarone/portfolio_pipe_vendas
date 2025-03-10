01bbc69c64bc4c9b21838e8bcd3823534cc3a674
LuizIavarone <luizfelipebaracat97@gmail.com>
2025-03-10 14:13:57 +0000

    ⭐ Feature - Criacao da dag de produtos, integracao para extracao da API e insercao no BigQuery

---
+++ DDLs/tb_raw_products.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE TABLE `portfolio-440702.raw.tb_raw_products` (
+  id STRING,
+  title STRING,
+  price STRING,
+  description STRING,
+  category STRING,
+  image STRING,
+  rating STRING,
+  dt_processamento DATETIME
+);
--- dags/dag_teste/dag_teste.py
+++ dags/dag_teste/dag_teste.py
@@ -3,19 +3,16 @@ from airflow.operators.python_operator import PythonOperator
 from datetime import datetime
 import subprocess
 
-# Função para rodar o dbt com o target dinâmico
 def run_dbt(target):
     command = f"bash -c 'source /home/admin/airflow_env/bin/activate && cd /home/admin/Projetos/pipe_vendas/dbt_pipe_vendas && dbt run --target {target}'"
     subprocess.run(command, shell=True, check=True)
 
-# Defina os argumentos padrão para as tasks
 default_args = {
     'owner': 'airflow',
     'retries': 1,
     'start_date': datetime(2025, 3, 10),
 }
 
-# Defina sua DAG
 dag = DAG(
     'dbt_dynamic_target_dag',
     default_args=default_args,
@@ -23,7 +20,6 @@ dag = DAG(
     catchup=False
 )
 
-# Defina as tarefas para rodar DBT com targets dinâmicos
 run_raw_target = PythonOperator(
     task_id='run_raw_target',
     python_callable=run_dbt,
@@ -45,5 +41,4 @@ run_refined_target = PythonOperator(
     dag=dag,
 )
 
-# Defina a ordem de execução
 run_raw_target >> run_trusted_target >> run_refined_target
